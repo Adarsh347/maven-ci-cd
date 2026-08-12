77a0ed888e5169dc4956254ea0038d0915ce4b7a
Adarsh Chandran kv <144888670+Adarsh347@users.noreply.github.com>
2026-08-12 10:49:37 +0530

    Add App class with greeting functionality

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+public class App {
+
+    public static String greet() {
+        return "Hello from Java CI/CD!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(greet());
+    }
+}
